146e4d187f8d583c9ff44f4afc877edf208deca4
Nícolas Almeida <99900335+Nicolas-Almeida-79@users.noreply.github.com>
2023-01-26 10:10:45 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,7 +1,7 @@
-create database EngSoft;
+create database if not exists EngSoft;
 use EngSoft;
 
-CREATE TABLE processador (
+CREATE TABLE IF NOT EXISTS cpu (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     modelo VARCHAR(255) NOT NULL,
     freqBase DECIMAL NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE processador (
     gpu_integrada BOOLEAN NOT NULL
 );
 
-CREATE TABLE gpu (
+CREATE TABLE IF NOT EXISTS gpu (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     modelo VARCHAR(255),
     memoria INT NOT NULL,
@@ -22,10 +22,21 @@ CREATE TABLE gpu (
     openGL DECIMAL NOT NULL
 );
 
-CREATE TABLE ram (
+CREATE TABLE IF NOT EXISTS ram (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     modelo VARCHAR(255) NOT NULL,
     capacidade INT NOT NULL,
     freq INT NOT NULL,
     latencia INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS jogo (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    cpu VARCHAR(255) NOT NULL,
+    gpu VARCHAR(255) NOT NULL,
+    ram VARCHAR(255) NOT NULL,
+    FOREIGN KEY (cpu) REFERENCES cpu (modelo),
+    FOREIGN KEY (gpu) REFERENCES gpu (modelo),
+    FOREIGN KEY (ram) REFERENCES ram (modelo)
 );
