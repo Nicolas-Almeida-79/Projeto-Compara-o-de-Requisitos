2ebcbe0ca903dcd9116726b3d0be95dd81967c74
Nícolas Almeida <99900335+Nicolas-Almeida-79@users.noreply.github.com>
2023-01-26 11:55:29 -0300

    Add files via upload

---
+++ cadastroUser.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Benchmark Games - Cadastro</title>
+    </head>
+    <body>
+        <form name="cadastro"  method="POST">
+            <fieldset>
+            <legend> Cadastrar </legend>
+                <label> Usuario: </label> <input type="text" name="nome" placeholder="Digite seu nome de usuario" required><br>
+                <label> Senha: </label> <input type="password" name="senha" placeholder="Digite sua senha" required><br>
+                <input type="submit" name="Enviar">
+            </fieldset>
+        </form>  
+    </body>
+</html>
