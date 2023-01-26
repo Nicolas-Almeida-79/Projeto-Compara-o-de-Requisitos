dc16bf52aa2524c17e019f80c7bef911c6e87abd
Nícolas Almeida <99900335+Nicolas-Almeida-79@users.noreply.github.com>
2023-01-26 10:34:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Benchmark Games</title>
+</head>
+<body>
+    <form>
+        <label for="jogos">Jogo:</label>
+        <select name="jogos" id="jogos"></select><br>
+        <label for="cpu">Processador:</label>
+        <select name="cpu" id="cpu"></select><br>
+        <label for="gpu">Placa de Video:</label>
+        <select name="gpu" id="gpu"></select><br>
+        <label for="ram">Memoria RAM:</label>
+        <select name="ram" id="ram"></select><br>
+        <input type="submit" name="Enviar">
+    </form>
+</body>
+</html>
